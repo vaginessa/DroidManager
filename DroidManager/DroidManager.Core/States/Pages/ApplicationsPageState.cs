7568e4d90bab0950497eab1fb52f1767845561a5
0xFireball <0xFireball@outlook.com>
2016-07-31 20:05:26 +0530

    list packages

---
+++ DroidManager/DroidManager.Core/States/Pages/ApplicationsPageState.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using SharpAdbClient.DeviceCommands;
+using System.Linq;
+
+namespace DroidManager.Core.States.Pages
+{
+    public class ApplicationsPageState
+    {
+        public ApplicationsPageState()
+        {
+            RefreshApplicationsInformation();
+        }
+
+        public List<string> InstalledPackageIds { get; private set; }
+        public Dictionary<string, string> InstalledPackages { get; private set; }
+
+        private void RefreshApplicationsInformation()
+        {
+            var currentDevice = AndroidDeviceConnection.OverviewState.CurrentDevice;
+            PackageManager pm = new PackageManager(currentDevice.DeviceMetadata);
+            pm.RefreshPackages();
+            InstalledPackages = pm.Packages;
+            InstalledPackageIds = InstalledPackages.Keys.ToList();
+        }
+    }
+}
--- DroidManager/DroidManager.Windows/VM/Pages/ApplicationsPageVM.cs
+++ DroidManager/DroidManager.Windows/VM/Pages/ApplicationsPageVM.cs
@@ -1,8 +1,37 @@
-﻿using NanoMvvm.Pagination;
+﻿using DroidManager.Core.States.Pages;
+using NanoMvvm;
+using NanoMvvm.Pagination;
+using System.Collections.Generic;
+using System.Windows.Input;
 
 namespace DroidManager.Windows.VM.Pages
 {
     internal class ApplicationsPageVM : SwitchablePageViewModel
     {
+        private ApplicationsPageState _pageState = new ApplicationsPageState();
+
+        public ICommand ReloadViewCommand => new DelegateCommand(ReloadView);
+
+        public List<string> InstalledPackageIdentifiers => _pageState.InstalledPackageIds;
+        string _selectedPackageId;
+
+        public string SelectedPackageId
+        {
+            get
+            {
+                return _selectedPackageId;
+            }
+
+            set
+            {
+                _selectedPackageId = value;
+                OnPropertyChanged(nameof(SelectedPackageId));
+            }
+        }
+
+        private void ReloadView(object obj)
+        {
+            PageView.SwitcherService.ReloadCurrentPage();
+        }
     }
 }
