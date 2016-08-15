dc9275819166716f81bf29ab7e1ef7099de8db86
0xFireball <0xFireball@outlook.com>
2016-08-15 18:39:50 +0530

    update some naming

--- DroidManager/DroidManager.Core/Classes/AndroidDeviceInformationService.cs
+++ DroidManager/DroidManager.Core/Classes/AndroidDeviceInformationService.cs
@@ -4,7 +4,7 @@
     {
         public AndroidDevice Device { get; }
         public string DeviceNameInformation => $"Device name: {Device.DeviceName}";
-        public string DeviceSerialInformation => $"Device serial identifier: {Device.DeviceMetadata.Serial}";
+        public string DeviceSerialInformation => $"Serial: {Device.DeviceMetadata.Serial}";
         public string ProductInformation => $"Product information: {Device.DeviceMetadata.Product}";
         public string DeviceFeatureInformation => $"Device features: {Device.DeviceMetadata.Features}";
         public string DeviceModelInformation => $"Device model: {Device.DeviceMetadata.Model}";
