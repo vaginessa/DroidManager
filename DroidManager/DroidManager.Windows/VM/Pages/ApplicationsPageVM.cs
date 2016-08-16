74e60f9d68ada24118d7ce99c898a2677ea7cba6
0xFireball <0xFireball@outlook.com>
2016-08-16 17:18:52 +0530

    add license headers

--- DroidManager/DroidManager.Windows/VM/Pages/ApplicationsPageVM.cs
+++ DroidManager/DroidManager.Windows/VM/Pages/ApplicationsPageVM.cs
@@ -1,4 +1,33 @@
-﻿using DroidManager.Core.CLIAdb;
+﻿#region Copyright and License Header
+/*
+
+	DroidManager
+
+	Copyright (c) 2016 0xFireball, IridiumIon Software
+
+	Author(s): 0xFireball
+	
+
+	This file is part of DroidManager.
+
+	DroidManager is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	DroidManager is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with DroidManager.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+#endregion
+
+
+using DroidManager.Core.CLIAdb;
 using DroidManager.Core.States.Pages;
 using MahApps.Metro.Controls;
 using MahApps.Metro.Controls.Dialogs;
